0f6ccba3e7e218514bcd83c5eb89c3d3901d66b2
Veera Reddy Konabhai <veerareddy908@gmail.com>
2023-04-17 22:38:08 +0530

    Time: 51 ms (99.73%), Space: 45.3 MB (64.28%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.js
+++ 125-valid-palindrome/125-valid-palindrome.js
@@ -3,7 +3,26 @@
  * @return {boolean}
  */
 var isPalindrome = function(s) {
-    let res = s.toLowerCase().replace(/[^a-z0-9]/gi,'');
-    let rev = res.split("").reverse().join("");
-    return res === rev;
+    //base case
+    if(s == null) return false;
+    if(s.trim() === "") return true;
+    //to convert into lower case
+    let str = s.toLowerCase();
+    let start = 0;
+    let end = str.length - 1;
+    while(start < end){
+        while(start < end && !((str.charAt(start) >= 'a' && str.charAt(start) <= 'z') || (str.charAt(start) >= '0' && str.charAt(start) <= '9'))){
+            start ++;
+        }
+        while(start < end && !((str.charAt(end) >= 'a' && str.charAt(end) <= 'z') || (str.charAt(end) >= '0' && str.charAt(end) <= '9'))){
+            end--;
+        }
+        if(str.charAt(start) != str.charAt(end)){
+            return false;
+        }
+        start++;
+        end--;
+        
+    }
+    return true;
 };
