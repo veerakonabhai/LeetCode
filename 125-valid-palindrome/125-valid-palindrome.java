033bd694c3182af29df483e2f6253a56eb57cd5d
Veera Reddy Konabhai <veerareddy908@gmail.com>
2023-04-17 21:23:03 +0530

    Time: 16 ms (48.34%), Space: 43.8 MB (20.37%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.java
+++ 125-valid-palindrome/125-valid-palindrome.java
@@ -1,24 +1,7 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        if(s.isEmpty()){
-            return true;
-        }
-        int start = 0;
-        int end = s.length() - 1;
-        while(start < end){
-            //to check any non alpha-numeric and skip it
-            while(start < end && !Character.isLetterOrDigit(s.charAt(start))){
-                start++;
-            }while(start < end && !Character.isLetterOrDigit(s.charAt(end))){
-                end--;
-            }
-            if(Character.toLowerCase(s.charAt(start)) != Character.toLowerCase(s.charAt(end))){
-                return false;
-            }
-            start++;
-            end--;
-        }
-        return true;
+        s = s.toLowerCase().replaceAll("[^a-z0-9]+", "");
+        return s.equals(new StringBuilder(s).reverse().toString());
 
     }
 }
