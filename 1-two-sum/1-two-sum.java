eb44f1e4264a7d274ffa68ebac611f8283f1fb08
Veera Reddy Konabhai <veerareddy908@gmail.com>
2023-04-18 20:57:30 +0530

    Time: 4 ms (63.87%), Space: 43.3 MB (13.23%) - LeetHub

--- 1-two-sum/1-two-sum.java
+++ 1-two-sum/1-two-sum.java
@@ -2,7 +2,7 @@ class Solution {
     public int[] twoSum(int[] nums, int target) {
         int[] result = new int[2];
         int k = nums.length;
-        boolean achieved = false;
+        /*boolean achieved = false;
         
         for(int i=0; i<k-1; i++){
             if(!achieved){
@@ -16,8 +16,21 @@ class Solution {
             }else{
                 break;
             }
+        }*/
+        HashMap<Integer, Integer> hm = new HashMap<>();
+        for(int i =0; i<k; i++){
+            hm.put(nums[i],i);
+        }
+        for(int j = 0; j<k;j++){
+            int i = target - nums[j];
+            if(hm.containsKey(i) && hm.get(i) != j){
+                result[0] = j;
+                result[1] = hm.get(i);
+                break;
+            }
         }
         
         return result;
+        
     }
 }
