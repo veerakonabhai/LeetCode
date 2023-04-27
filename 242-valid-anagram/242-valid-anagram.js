7c257ccb708dc5dd4f52d475389c83552f263b8a
Veera Reddy Konabhai <veerareddy908@gmail.com>
2023-04-27 23:13:39 +0530

    Time: 79 ms (67.72%), Space: 43 MB (79.91%) - LeetHub

---
+++ 242-valid-anagram/242-valid-anagram.js
@@ -0,0 +1,26 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isAnagram = function(s, t) {
+    //base case
+    if(s.length !== t.length) return false;
+    
+    //using map and for loops to iterate over
+    let map = {};
+    for(let i = 0; i < s.length; i++){
+        let letter = s[i];
+        if(!map[letter]){
+            map[letter] = 1;
+        }else{
+            map[letter]++;
+        }
+    }
+    for(let i = 0; i < t.length; i++){
+        let letter = t[i];
+        if((!map[letter]) || map[letter] < 1)  return false;
+        map[letter]--;
+    }
+    return true;
+};
