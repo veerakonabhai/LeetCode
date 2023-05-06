38a3c2a2bd6cabf62bc86da8c3669223e976f4d1
Veera Reddy Konabhai <veerareddy908@gmail.com>
2023-05-06 18:48:38 +0530

    Time: 63 ms (33.53%), Space: 41.7 MB (56.59%) - LeetHub

--- 278-first-bad-version/278-first-bad-version.js
+++ 278-first-bad-version/278-first-bad-version.js
@@ -1,20 +1,28 @@
-/* The isBadVersion API is defined in the parent class VersionControl.
-      boolean isBadVersion(int version); */
+/**
+ * Definition for isBadVersion()
+ * 
+ * @param {integer} version number
+ * @return {boolean} whether the version is bad
+ * isBadVersion = function(version) {
+ *     ...
+ * };
+ */
 
-public class Solution extends VersionControl {
-    public int firstBadVersion(int n) {
-        if(isBadVersion(1)){
-            return 1;
-        }
-        int left = 1, right = n;
+/**
+ * @param {function} isBadVersion()
+ * @return {function}
+ */
+var solution = function(isBadVersion) {
+    /**
+     * @param {integer} n Total versions
+     * @return {integer} The first bad version
+     */
+    return function(n) {
+        let left = 1, right = n;
         while(left < right){
-            int mid = left + (right-left)/2;
-            if(isBadVersion(mid)){
-                right = mid;
-            }else{
-                left = mid + 1;
-            }
+            let mid = Math.floor(left + (right - left)/2);
+            isBadVersion(mid) ? right = mid : left = mid+1;
         }
         return left;
-    }
-}
+    };
+};
