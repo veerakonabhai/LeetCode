2ca3a662aa02274cf85f08e834abcfe661c3182e
Veera Reddy Konabhai <veerareddy908@gmail.com>
2023-05-07 12:42:11 +0530

    Time: 71 ms (38.48%), Space: 44 MB (99.19%) - LeetHub

---
+++ 2665-counter-ii/2665-counter-ii.js
@@ -0,0 +1,21 @@
+/**
+ * @param {integer} init
+ * @return { increment: Function, decrement: Function, reset: Function }
+ */
+var createCounter = function(init) {
+    let n = init;
+    return {
+        increment : () => {return ++n;},
+        decrement : () => {return --n;},
+        reset : () => {n = init;
+            return n;}
+        
+    }
+};
+
+/**
+ * const counter = createCounter(5)
+ * counter.increment(); // 6
+ * counter.reset(); // 5
+ * counter.decrement(); // 4
+ */
