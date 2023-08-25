3d84bf0f8861c9790a6b61cfad0e18d9f942062d
Veera Reddy Konabhai <veerareddy908@gmail.com>
2023-08-25 20:49:51 +0530

    Time: 690 ms (96.78%), Space: 0B (100.00%) - LeetHub

--- 0197-rising-temperature/0197-rising-temperature.sql
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -1,3 +1,5 @@
 # Write your MySQL query statement below
-SELECT w.id FROM Weather w, Weather w1
-WHERE w.temperature > w1.temperature AND dATEDIFF(w.recordDate, w1.recordDate) = 1
+select today.id from Weather today, Weather yesterday
+where 
+	today.temperature > yesterday.temperature
+	and yesterday .recordDate = DATE_SUB(today.recordDate, INTERVAL 1 DAY)
